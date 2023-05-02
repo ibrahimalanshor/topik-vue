d9b0f18ef5a863da2d4f90a36d4e15d97507b6b2
ibra <ibrahimalanshor6@gmail.com>
2023-05-02 13:33:57 +0800

    feat: init prototype

--- src/pages/index-page.vue
+++ src/pages/index-page.vue
@@ -1,3 +1,14 @@
 <template>
-  <div>Index</div>
+  <div class="container mx-auto sm:px-6 lg:px-8 h-screen pt-4">
+    <div class="min-h-screen">dsaa</div>
+    <div class="sticky left-0 bottom-4">
+      <input
+        type="email"
+        name="email"
+        id="email"
+        class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+        placeholder="Message"
+      />
+    </div>
+  </div>
 </template>
