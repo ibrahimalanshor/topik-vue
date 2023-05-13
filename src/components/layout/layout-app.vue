b5904950bea3b09057ca40c53fee020865758955
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2023-05-13 14:04:21 +0700

    feat: resource

--- src/components/layout/layout-app.vue
+++ src/components/layout/layout-app.vue
@@ -1,11 +1,11 @@
 <script setup>
-import PartialLayoutSidebar from '@/components/partial/layout/partial-layout-sidebar.vue';
+import PartialSidebar from '@/components/partial/sidebar/partial-sidebar.vue';
 </script>
 
 <template>
   <div class="flex">
     <div class="h-screen lg:inset-y-0 lg:z-50 lg:flex lg:w-72 lg:flex-col">
-      <partial-layout-sidebar />
+      <partial-sidebar />
     </div>
 
     <div class="w-full">
---
+++ src/components/partial/sidebar/partial-sidebar-topic-list.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { useString } from '@/composes/resource.compose';
+import PartialSidebarTopicItem from './partial-sidebar-topic-item.vue';
+
+const { getString } = useString();
+
+const topics = [
+  { id: 1, name: 'Heroicons' },
+  { id: 2, name: 'Tailwind Labs' },
+  { id: 3, name: 'Workcation' },
+];
+</script>
+
+<template>
+  <div class="text-xs font-semibold leading-6 text-gray-400">
+    {{ getString('sidebar.label.topic-list') }}
+  </div>
+  <div role="list" class="-mx-2 mt-2 space-y-1">
+    <partial-sidebar-topic-item
+      v-for="topic in topics"
+      :key="topic.id"
+      :topic="topic"
+    />
+  </div>
+</template>
