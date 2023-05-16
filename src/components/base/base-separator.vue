63848dcced6cbaa12df2576d253f1ba39ca8bf8f
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2023-05-17 05:53:07 +0700

    feat: base separator

---
+++ src/components/base/base-separator.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="block relative">
+    <div class="absolute inset-0 w-full flex items-center">
+      <div class="w-full border-t"></div>
+    </div>
+    <div class="relative flex items-center justify-center">
+      <span class="bg-white px-6 font-light text-gray-500 text-sm">
+        <slot />
+      </span>
+    </div>
+  </div>
+</template>
