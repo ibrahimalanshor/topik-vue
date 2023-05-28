2e8214a470bd483f3520d7ae0200481b0413729b
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2023-05-28 14:17:22 +0700

    feat: axios token interceptor

--- src/common/axios.js
+++ src/common/axios.js
@@ -1,5 +1,26 @@
 import axios from 'axios';
+import { router } from '@/router/router';
+import { useAuthStore } from '@/store/modules/auth.store';
+import { isLowerThanNow } from '@/common/utils/date.util';
 
-export default axios.create({
+const http = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
 });
+
+http.interceptors.request.use(async (config) => {
+  const authStore = useAuthStore();
+
+  if (
+    !authStore.isLoggedIn ||
+    isLowerThanNow(authStore.getDecoded().exp * 1000)
+  ) {
+    authStore.logout();
+    router.push({ name: 'login' });
+  }
+
+  config.headers['Authorization'] = authStore.token;
+
+  return config;
+});
+
+export default http;
